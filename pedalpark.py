f14de73aafe9bd56a0de610006dddc6a5f19eb8c
Caroline Van den Hauwe <caroline.van.den.hauwe@gmail.com>
2014-04-07 10:01:50 -0400

    Fixed issue where MongoDB couldn't accept JSON data invalid for geoindex if a previous index existed, by removing index before insertion.

--- pedalpark.py
+++ pedalpark.py
@@ -2,16 +2,21 @@ from flask import Flask, render_template, request
 from flask.ext.pymongo import PyMongo
 from pymongo import GEO2D
 import requests
+import sys
 
 app = Flask(__name__)
 
 """ ~ Database methods ~ """
 mongo = PyMongo(app)
 
+def empty_db(c):
+	"""Remove all data and indexes from collection"""
+	c.remove()
+	c.drop_indexes()
+
 @app.route('/db/update')
 def update_db():
-	"""Empty DB"""
-	mongo.db.parkings.remove()
+	empty_db(mongo.db.parkings)	
 	print "Removed all known bike parkings."
 
 	"""Fill DB with entries from paginated API"""
@@ -29,30 +34,32 @@ def update_db():
 		resp_size = len(resp_json)
 		print "Received %d parkings." % (resp_size)
 		if resp_size > 0:
-		    for resp_item in resp_json: cast_for_latlong(resp_item)
+		    for resp_item in resp_json: cast_latlong(resp_item)
 		    print "Casted all latitude and longitude fields."
-		    mongo.db.parkings.insert(resp_json)
-		    print "Inserted into database."
+		    import_size += insert_into_db(mongo.db.parkings,resp_json)
 		    offset += pagesize
-		    import_size += resp_size
 		    print "Importing... %d bike parkings so far." % import_size
 		else:
 		    end_of_data = True
 
-	"""remove coordinates attribute, for MongoDB GeoSpatial Indexing"""
-	mongo.db.parkings.update({},{'$unset': {'coordinates.needs_recoding':1}}, multi=True)
-	print "MongoDB needs_recoding removed."
+	prepare_db_for_geo(mongo.db.parkings)	    
 
 	print "%d bike parkings imported." % (import_size)
 	return render_template('start.html')
+ 
+def insert_into_db(c,item):
+	try:
+		c.insert(item)
+		return len(item)
+	except:
+		print "Unexpected error when inserting into db:", sys.exc_info()
 
-@app.route('/db/index')
-def ensure_index():
-	"""Create index for MongoDB's GeoSpatial Indexing"""
-	mongo.db.parkings.ensure_index([("coordinates", GEO2D)])
-	return render_template('start.html')
+def prepare_db_for_geo(c):
+	"""Remove coordinates attribute and create index for MongoDB GeoSpatial Indexing"""
+	c.update({},{'$unset': {'coordinates.needs_recoding':1}}, multi=True)
+	c.ensure_index([("coordinates", GEO2D)])
 
-def cast_for_latlong(item):
+def cast_latlong(item):
     """MongoDB GeoSpatial Indexing requires float values of latitude and longitude"""
     item["coordinates"]["latitude"] = float(item["coordinates"]["latitude"])
     item["coordinates"]["longitude"] = float(item["coordinates"]["longitude"])
