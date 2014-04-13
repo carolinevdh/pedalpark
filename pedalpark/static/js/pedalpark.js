4e00857c396c58d1c0808daa7db33e9991d2a816
Caroline Van den Hauwe <caroline.van.den.hauwe@gmail.com>
2014-04-13 00:45:33 -0400

    UX/UI for various non-ideal cases, regarding location and database state.

--- pedalpark/static/js/pedalpark.js
+++ pedalpark/static/js/pedalpark.js
@@ -16,24 +16,31 @@
 
 	// - MODEL
 
-	var BikeParkingsModel = Backbone.Model.extend({
+	var NearBikeParkingsModel = Backbone.Model.extend({
 		url: function() {
 			return '/near';
 		}
 	});
 
+	var AllBikeParkingsModel = Backbone.Model.extend({
+		url: function() {
+			return '/all';
+		}
+	});
+
 	var UserLocationModel = Backbone.Model.extend({
 		initialize: function() {
-			_.bindAll(this,'positionSuccess','updateLocation');
+			_.bindAll(this,'updateLocation','positionSuccess','positionFailure');
 			this.updateLocation();
 		},
 
 		updateLocation: function() {
 			if(!FAKE_SF_LOCATION){
 				if(navigator.geolocation)
-					navigator.geolocation.getCurrentPosition(this.positionSuccess);
+					navigator.geolocation.getCurrentPosition(this.positionSuccess, this.positionFailure);
 			}else{
 				this.set({
+					success: true,
 					latitude : 37.390947 + Math.random(),
 					longitude : -122.393171
 				});
@@ -43,6 +50,7 @@
 
 		positionSuccess: function(position) {
 			this.set({
+				success : true,
 				latitude : position.coords.latitude,
 				longitude : position.coords.longitude
 			});
@@ -50,13 +58,16 @@
 		},
 
 		positionFailure: function() {
-			console.log('ERROR: Could not acquire new position, still: (' + this.get('latitude') + ", " + this.get('longitude') + ').');
+			this.set({
+				success : false
+			});
 		}
 	});
 
 	// - VIEW
 
 	var BikeParkingsView = Backbone.View.extend({
+		el: $('#bikeparkings').first(),
 		initialize: function(){
 			this.template = _.template($('#bikeparkings-template').html());
 			this.listenTo(this.model,'change', this.render);
@@ -67,45 +78,38 @@
 	});
 
 	var MapView = Backbone.View.extend({
-		el : $('#map-canvas').first(),
-		render: function(){
-			return this;
-		},
-		getMapOptions: function(lat, long){
-			return mapOptions = {
-				zoom: 1,
-				center: new google.maps.LatLng(lat,long),
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-		},
-		update: function(lat, long, parkinglocations){
+		el : $('#map-canvas'),
+
+		update: function(doMarkLocation, doCenter, doFitBounds, lat, long, parkinglocations){
 			//Build Google Map for drawing
-			mapOptions = this.getMapOptions(lat,long);
-			map = new google.maps.Map(this.el,mapOptions);
+			map = new google.maps.Map(this.el, { zoom : 1});
+			mainLatLng = new google.maps.LatLng(lat, long);
 			latLngBounds = new google.maps.LatLngBounds();
-			currentMarker = new google.maps.Marker({
-				position : mapOptions.center,
-				map : map,
-				icon : 'static/img/marker-cyclist.png'
-			});
-			latLngBounds.extend(mapOptions.center);
-			for ( i = 0; i < parkinglocations.length; i++ ){
-				console.log(parkinglocations[i]);
+			if ( doMarkLocation ) {
+				currentMarker = new google.maps.Marker({
+					position : mainLatLng,
+					map : map,
+					icon : 'static/img/marker-cyclist.png'
+				});
+				latLngBounds.extend(mainLatLng);
+			}
+			nLocations = parkinglocations.length;
+			for ( i = 0; i < nLocations; i++ ){
 				latLng = new google.maps.LatLng(
 						parkinglocations[i].coordinates.latitude,
 						parkinglocations[i].coordinates.longitude);
 				parkingMarker = new google.maps.Marker({
 					position : latLng,
 					map : map,
-					icon : 'static/img/marker-parking-' + (i + 1) + '.png'
+					icon : this.getMarkerIcon(nLocations, i+1)
 				});
 				latLngBounds.extend(latLng);
 			}
 			bikeLayer = new google.maps.BicyclingLayer();
 			bikeLayer.setMap(map);
 
-			map.setCenter(latLngBounds.getCenter());
-			map.fitBounds(latLngBounds); 
+			if ( doCenter )	map.setCenter(latLngBounds.getCenter());
+			if ( doFitBounds ) map.fitBounds(latLngBounds);
 
 			//Listen to resize events: make Google Map responsive
 			var center;
@@ -116,11 +120,36 @@
 				calculateCenter();
 			});
 			google.maps.event.addDomListener(window, 'resize', function() {
-				map.setCenter(center);
+				if( doCenter ) map.setCenter(center);
 			});
 
 			//Finally, draw Google Map
 			this.render();
+		},
+
+		renderWorld: function(){
+			this.update(false,true,false,0,0,[]);
+		},
+
+		getMarkerIcon: function(size, index){
+			if ( size < 10) return 'static/img/marker-parking-' + index + '.png';
+			else return 'static/img/marker-parking.png';
+		},
+
+		makeResponsive: function(map){
+			
+		}
+	});
+
+	var NoticeView = Backbone.View.extend({
+		el : $('#notice').first(),
+		initialize: function(){
+			_.bindAll(this,'render');
+			this.template = _.template($('#notice-template').html());
+			this.listenTo(this.model,'change', this.render);
+		},
+		render: function(notice){
+			this.$el.html(this.template({ 'notice' : notice }));
 		}
 	});
 
@@ -128,14 +157,15 @@
 
 	var ParkingsRouter = Backbone.Router.extend({
 		initialize: function() {
-			_.bindAll(this,'onLocationUpdate','parkingFetchSuccess');
+			_.bindAll(this,'onLocationUpdate','nearParkingFetchSuccess','allParkingFetchSuccess','parkingFetchError');
+
 			this.userLocationModel = new UserLocationModel();
-			this.bikeParkingsModel = new BikeParkingsModel();
-			this.bikeParkingsView = new BikeParkingsView({
-				el: $('#bikeparkings').first(),
-				model: this.bikeParkingsModel
-			});
+			this.nearBikeParkingsModel = new NearBikeParkingsModel();
+
 			this.mapView = new MapView();
+			this.bikeParkingsView = new BikeParkingsView({ model : this.nearBikeParkingsModel });
+			this.noticeView = new NoticeView({ model : this.nearBikeParkingsModel });
+
 			if(!FAKE_SF_LOCATION)
 				this.listenTo(this.userLocationModel, 'change', this.onLocationUpdate);
 			else{
@@ -148,28 +178,63 @@
 		},
 
 		onLocationUpdate: function(model){
-			console.log('ParkingsRouter received userLocationModel change.');
-			//Fetch new bike parkings
-			this.bikeParkingsModel.fetch({
-				data : {
-					lat : model.get('latitude'),
-					long : model.get('longitude'),
-					limit : 4
-				},
-				success : this.parkingFetchSuccess
-			});
+			//If a location exists for the user
+			if( model.get('success') ) {
+				//Fetch new bike parkings
+				this.nearBikeParkingsModel.fetch({
+					data : {
+						lat : model.get('latitude'),
+						long : model.get('longitude'),
+						limit : 4
+					},
+					success : this.nearParkingFetchSuccess,
+					error : this.parkingFetchError
+				});
+			//If no location exists for the user
+			}else{
+				//Fetch all bike parkings
+				this.allBikeParkingsModel = new AllBikeParkingsModel();
+				this.allBikeParkingsModel.fetch({
+					success : this.allParkingFetchSuccess,
+					error : this.parkingFetchError
+				});
+			}
 		},
 
-		parkingFetchSuccess: function(){
-			console.log('Successfully fetched bikeparkings');
+		nearParkingFetchSuccess: function(){
+			if( !this.nearBikeParkingsModel.get('success') )
+				this.parkingFetchError();
+			else {
+				this.noticeView.render('Hi! Here are some bicycle parkings, close to your current location. Pick one to get directions!');
 
-			//Update Google Map with new current location
-			this.mapView.update(
-				this.userLocationModel.get('latitude'),
-				this.userLocationModel.get('longitude'),
-				this.bikeParkingsModel.get('locations')
-			);
-		}
+				//Update Google Map with new current location and bike parkings
+				this.mapView.update(
+					true,true,true,
+					this.userLocationModel.get('latitude'),
+					this.userLocationModel.get('longitude'),
+					this.nearBikeParkingsModel.get('locations')
+				);
+			}
+		},
+
+		allParkingFetchSuccess: function(){
+			if( !this.allBikeParkingsModel.get('success') )
+				this.parkingFetchError();
+			else {
+				this.noticeView.render('Oops, we couldn\'t find your location. Either way, here are all bicycle parkings in San Francisco! Please enable location awareness in your browser.');
+
+				//Render a map with all known bicycle parkings
+				this.mapView.update(false,true,true,0,0,this.allBikeParkingsModel.get('locations'));
+			}
+		},
+
+		parkingFetchError: function(){
+			console.log('parkingFetchError');
+			this.noticeView.render('Oops, we couldn\t find any bicycle parkings, nor your location. Please enable location awareness in your browser.');
+
+			//Render a map of the world
+			this.mapView.renderWorld();
+		},
 	});
 
 }());
